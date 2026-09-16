16876c0b1af8266e22edc7380f54319621ce7ff3
a7mad2702 <ahmad_eme@yahoo.com>
2026-09-16 13:33:37 +0300

    Add script to decline pending posts in Facebook group
    This script automatically declines pending posts in a Facebook group every 6 minute. It requires admin access and uses the browser console to execute.

---
+++ declinePendingPosts.js
@@ -0,0 +1,38 @@
+// ---- decline pending posts
+
+// you have to be admin on the group to run this script on your browser console
+// 1- open your group spam ===> https://www.facebook.com/groups/PutHereGroupIDNumber/pending_posts/
+// 2- open the browser console then copy and paste this script 
+// 3- this will run every 6 seconds to finish deleting all pending posts
+
+let timerId = 0;
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function declinePosts() {
+  try {
+    console.log("🧹 Starting :::decline pending posts:::...timerId:", timerId);
+    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+    await sleep(1000);
+    
+
+const selectAll = await document.querySelectorAll('[aria-label*="Select all"]')[0];
+if(selectAll){
+      selectAll.click();
+   }
+await sleep(2000); 
+var declineButton = Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Decline' );
+if(declineButton){
+    declineButton.click();
+    await sleep(1000);
+    var doneButton = Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Done' );
+      if(doneButton)doneButton.click();
+
+}
+} catch (error) {
+    console.error("❌ Error processing posts:", error);
+  } finally {
+    // Schedule the next run only AFTER the current one completely finishes
+    timerId = setTimeout(declinePosts, 60000); 
+  }
+}
+declinePosts();
