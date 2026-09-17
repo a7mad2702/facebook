5718e6119f5a47f803ac5fa35c9d6008c11d65a1
a7mad2702 <ahmad_eme@yahoo.com>
2026-09-17 15:25:48 +0300

    Implement random timing for post deletion
    Updated the script to use random intervals for deleting posts instead of a fixed schedule. Introduced min and max limits for both minutes and seconds to enhance the randomness of execution.

--- removePublishedPosts.js
+++ removePublishedPosts.js
@@ -3,9 +3,17 @@
 // you have to be admin on the group ro run this script on your browser console
 // 1- open your group spam ===> https://www.facebook.com/groups/PutHereGroupIDNumber/
 // 2- open the brwoser console then copy and paste this script 
-// 3- this will run every 3 minutes to finish deleting all published posts, (you can change the value of scheduleMinutes)
+// 3- this will run every RANDOM minutes to finish deleting all published posts, (you can change the value of scheduleMinutes)
 
-let scheduleMinutes = 3;
+
+let maxMinutes =7;
+let minMinutes= 3;
+
+
+let maxSeconds =3;
+let minSeconds= 1;
+let randSeconds = 1;
+
 
 let timerId = 0;
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -20,22 +28,26 @@ let cnt = menuButtons.length;
   for (let mnuBtn of menuButtons) {
       console.log(`Processing ${index} of ${cnt}`); 
      await mnuBtn.click();
-     await sleep(1000); 
+     randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+     await sleep(randSeconds); 
     var deleteButton = await Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Remove post' || el.textContent === 'Delete post');
     if (deleteButton) {
         deleteButton.click();
         console.log('Remove clicked');
-        await sleep(1000); 
+        randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+        await sleep(randSeconds); 
         var confirmButton = await Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Confirm' || el.textContent === 'Delete');
           if (confirmButton) {
             await confirmButton.click();
             console.log('Confirm clicked');
-            await sleep(1000); 
+            randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+            await sleep(randSeconds); 
             var deleteBtnLastConfirm = await Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Confirm' || el.textContent === 'Delete');
           if (deleteBtnLastConfirm) 
             {
               await deleteBtnLastConfirm.click();
-              await sleep(1000); 
+              randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+              await sleep(randSeconds); 
               console.log('Last Confirm clicked');
             }
              else{ console.log('No LastConfirm Button Found'); }
@@ -51,12 +63,18 @@ let cnt = menuButtons.length;
   } finally {
     // Schedule the next run only AFTER the current one completely finishes
     window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
-    await sleep(2000);
+    randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+    await sleep(randSeconds);
 
     window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
-    await sleep(3000);
+    randSeconds = (Math.floor(Math.random() * (maxSeconds - minSeconds + 1)) + minSeconds)*1000;
+    await sleep(randSeconds);
 
-    let milliseconds = 1000 * 60 * scheduleMinutes;
+    let randMinutes = Math.floor(Math.random() * (maxMinutes - minMinutes + 1)) + minMinutes;
+
+    console.log(`I'll run again after: ${randMinutes} of Minutes`);
+
+    let milliseconds = 1000 * 60 * randMinutes;
 
     timerId = setTimeout(cleanPosts, milliseconds); 
   }
